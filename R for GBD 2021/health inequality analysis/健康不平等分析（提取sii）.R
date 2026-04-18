8af021012aa05e8aa65bdaf38e7319b2da8e7739
E1H-zy <pfkzhuyue@163.com>
2026-04-18 20:29:15 +0800

    extract for SII.R

--- R for GBD 2021/health inequality analysis/健康不平等分析（提取sii）.R
+++ R for GBD 2021/health inequality analysis/健康不平等分析（提取sii）.R
@@ -1,4 +1,4 @@
-# 提取斜率指数（SII）及其95%置信区间
+
 sii_table <- data.frame(
   Year = c(1990, 2021),
   SII = c(coef(r.huber1)[2], coef(r.huber2)[2]),
@@ -6,29 +6,25 @@ sii_table <- data.frame(
   CI_upper = c(confint.default(r.huber1)[2, 2], confint.default(r.huber2)[2, 2])
 )
 
-# 保留两位小数
 sii_table <- sii_table %>%
   mutate(across(SII:CI_upper, ~ round(.x, 2)))
 
-# 查看表格
 print(sii_table)
 
-#####################################  05-01每年sii修改版############################
-setwd("F:\\table+figer资料")
+
+setwd("F:\\")
 #install.packages("ggbrace")
-# 加载需要的包 ------------------------------------------------------------------
+
 library(tidyverse)
 library(data.table)
-library(car)# 异方差诊断
-library(MASS)# 稳健回归
-library(mgcv)# 提供洛伦兹曲线拟合 (样条函数等)
-library(splines)# 拟合样条函数
+library(car)
+library(MASS)
+library(mgcv)
+library(splines)
 library(broom)
 library(ggplot2)
-# 数据准备--------------------------------------------------------------------
 
-# 读取人口数据
-path = "F:\\table+figer资料\\GBD_population"
+path = "F:\\"
 fileName = dir(path)
 fileName
 population <- data.frame()
@@ -43,13 +39,12 @@ for(k in 1:length(fileName)){
 
 pop1 <- population %>%
   dplyr::select(location_name,sex_name,age_name,year,metric_name,val)
-# 读取并合并三个数据
-# 疾病负担数据
-data1<- vroom::vroom("F:\\table+figer资料\\Urticaria-ditu.csv")
+
+data1<- vroom::vroom("F:\\")
 data2 <- subset(data1,data1$measure_name=="DALYs (Disability-Adjusted Life Years)"&
                   data1$sex_name=="Both"&
                   data1$age_name=="10-24 years")
-#03-16修改版
+
 data2$location_name[data2$location_name == "Republic of the Union of Myanmar"] = 'Myanmar'
 data2$location_name[data2$location_name == "Republic of Tajikistan"] = 'Tajikistan'
 data2$location_name[data2$location_name == "Republic of the Philippines"] = 'Philippines'
@@ -80,9 +75,9 @@ data2$location_name[data2$location_name == "Republic of the Philippines"] = 'Phi
 data2$location_name[data2$location_name == "Brunei Darussalam"] = 'Brunei Darussalam'
 
 
-# sdi 数据
-sdi <- read.csv("SDI2021.csv",header = T,check.names = F)
-sdi <- sdi %>% # 宽数据转为长数据
+
+sdi <- read.csv("",header = T,check.names = F)
+sdi <- sdi %>%
   pivot_longer(cols = `1990`:`2021`,names_to = "year") %>%
   rename(sdi=value) %>%
   dplyr::select(location,year,sdi)
@@ -99,41 +94,30 @@ pop1 <- pop1 %>%
   dplyr::select("location_name","sex_name","year","val") %>%
   rename(pop=val)
 
-
-
-
-
-# 合并人口数据, 生成 mydata
 mydata <- left_join(data,pop1,
                     by=c("location_name","sex_name","year"))
 
 unique(mydata$metric_name)
 
 library(dplyr)
-library(MASS)  # rlm回归
-
-# 计算每年的斜度指数（SII）
-
+library(MASS)  
 rank <- mydata %>%
   group_by(year, metric_name) %>%
   arrange(sdi) %>%
-  mutate(pop_global = sum(pop),  # 计算每年总人口
-         cummu = cumsum(pop),   # 累积人口
-         half = pop / 2,         # 该国家人口的一半
-         midpoint = cummu - half, # 计算人口的中点
-         weighted_order = midpoint / pop_global) # 计算相对位置
+  mutate(pop_global = sum(pop),  
+         cummu = cumsum(pop),  
+         half = pop / 2,       
+         midpoint = cummu - half,
+         weighted_order = midpoint / pop_global)
 
-# 过滤"Rate"指标并按年份单独做回归
 result <- rank %>%
   filter(metric_name == "Rate") %>%
   group_by(year) %>%
   do({
-    # 每一年做回归
     fit <- rlm(val ~ weighted_order, data = .)
-    coef_vals <- coef(fit)  # 提取回归系数
-    conf_interval <- confint(fit)  # 计算95%可信区间
+    coef_vals <- coef(fit) 
+    conf_interval <- confint(fit)
     
-    # 提取回归系数和可信区间
     tibble(year = unique(.$year),
            intercept = coef_vals[1],
            slope = coef_vals[2],
@@ -141,7 +125,6 @@ result <- rank %>%
            CI_upper = conf_interval[2, 2])
   })
 
-# 打印结果
 print(result)
 #write.csv(result, "SII_results.csv", row.names = FALSE)
 
