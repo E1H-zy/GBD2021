c89e44f4280481dbc1f2a31e46710be0bcfad747
E1H-zy <pfkzhuyue@163.com>
2026-04-18 20:11:33 +0800

    ARIMA.R

--- R for GBD 2021/ARIMA/ARIMA.R
+++ R for GBD 2021/ARIMA/ARIMA.R
@@ -1,6 +1,5 @@
 
-setwd("F:\\我的工作文件夹\\11 ARIMA")
-# 安装并加载必要的包
+setwd("F:\\")
 #install.packages("forecast")
 #install.packages("readxl")
 library(forecast)
@@ -12,31 +11,24 @@ library(ggpubr)
 library(tseries)
 
 
-#####男性ASIR 导入数据#####
 male_in<-read_excel("ARIMA.xlsx",sheet=2)
 
-# 将数据转换为时间序列对象
 male_in_ts <- ts(male_in$val, start = 1990, frequency = 1)
 
-# 检查数据
 plot(male_in_ts)
 kpss.test(male_in_ts)
 
-# 拟合ARIMA模型
-fit1 <- auto.arima(male_in_ts)   ###自动拟合出最佳的p,d,q的值
-fit1                             ###查看p,d,q的值
-summary(fit1)                    ###查看模型的AIC,BIC的指标
+fit1 <- auto.arima(male_in_ts)   
+fit1                             
+summary(fit1)                   
 
-# 进行预测
-forecasted_values1 <- forecast(fit1, h = 15) ###预测未来15年数据
-# 将预测结果转换为数据框
+forecasted_values1 <- forecast(fit1, h = 15)
 forecast_df1 <- data.frame(             
   Year = c(time(male_in_ts), time(forecasted_values1$mean)),
   Value = c(as.numeric(male_in_ts), as.numeric(forecasted_values1$mean)),
   Type = c(rep("Actual", length(male_in_ts)), rep("Forecast", length(forecasted_values1$mean)))
 )
 
-# 绘制预测结果
 
 p1<-ggplot() +
   geom_line(data = forecast_df1, aes(x = Year, y = Value, color = Type), size = 1.2) +
@@ -61,31 +53,23 @@ p1<-ggplot() +
 p1
 
 
-
-#####女性ASIR导入数据#####
 female_in<-read_excel("ARIMA.xlsx",sheet=1)
 
-# 将数据转换为时间序列对象
 female_in_ts <- ts(female_in$val, start = 1990, frequency = 1)
 
-# 检查数据
 plot(female_in_ts)
 
-# 拟合ARIMA模型
 fit2 <- auto.arima(female_in_ts)
 fit2
 summary(fit2)
 
-# 进行预测
 forecasted_values2 <- forecast(fit2, h = 15)
-# 将预测结果转换为数据框
 forecast_df2 <- data.frame(
   Year = c(time(female_in_ts), time(forecasted_values2$mean)),
   Value = c(as.numeric(female_in_ts), as.numeric(forecasted_values2$mean)),
   Type = c(rep("Actual", length(female_in_ts)), rep("Forecast", length(forecasted_values2$mean)))
 )
 
-# 绘制预测结果
 
 p2<-ggplot() +
   geom_line(data = forecast_df2, aes(x = Year, y = Value, color = Type), size = 1.2) +
@@ -106,30 +90,23 @@ p2<-ggplot() +
         plot.margin = margin(10, 10, 10, 10))
 p2
 
-#####Both ASIR导入数据#####
 both_in<-read_excel("ARIMA.xlsx",sheet=3)
 
-# 将数据转换为时间序列对象
 both_in_ts <- ts(both_in$val, start = 1990, frequency = 1)
 
-# 检查数据
 plot(both_in_ts)
 
-# 拟合ARIMA模型
 fit3 <- auto.arima(both_in_ts)
 fit3
 summary(fit3)
 
-# 进行预测
 forecasted_values3 <- forecast(fit3, h = 15)
-# 将预测结果转换为数据框
 forecast_df3 <- data.frame(
   Year = c(time(both_in_ts), time(forecasted_values3$mean)),
   Value = c(as.numeric(both_in_ts), as.numeric(forecasted_values3$mean)),
   Type = c(rep("Actual", length(both_in_ts)), rep("Forecast", length(forecasted_values3$mean)))
 )
 
-# 绘制预测结果
 
 p3<-ggplot() +
   geom_line(data = forecast_df3, aes(x = Year, y = Value, color = Type), size = 1.2) +
@@ -151,16 +128,11 @@ p3<-ggplot() +
 p3
 
 
-
-
-# 安装并加载 gridExtra 包
 #install.packages("gridExtra")
 library(gridExtra)
 
-# 假设 p1, p2, p3 是三个 ggplot 对象
-grid.arrange(p1, p2, p3, ncol = 3)  # 横排排列
-# 调整各个图形的宽度
-grid.arrange(p3,p1, p2,  ncol = 3, widths = c(1, 1, 1))  # 可调整每个图形的宽度比例
+grid.arrange(p1, p2, p3, ncol = 3) 
+grid.arrange(p3,p1, p2,  ncol = 3, widths = c(1, 1, 1)) 
 ggsave("ARIMA.pdf", grid.arrange(p3,p1, p2, ncol = 3), width = 14, height = 3,dpi = 300)
 
 
